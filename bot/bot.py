4e94e05c40268bdb4cfcb3f34ec64a52e7617998
Aro <aslanyan.p.aro@gmail.com>
2019-10-07 01:52:05 +0400

    add command music for sending music

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ from subprocess import PIPE
 from subprocess import Popen
 import config
 import telebot
+import os, time
 
 bot = telebot.TeleBot(config.token)
 
@@ -22,9 +23,14 @@ def date_time(message):
     )
 
 
-@bot.message_handler()
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['music'])
+def send_music(message):
+    for file in os.listdir("music/"):
+        if file.split(".")[-1] == "ogg":
+            f = open("music/" + file, "rb")
+            msg = bot.send_voice(message.chat.id, f, None)
+            bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
+        time.sleep(3)
 
 
 if __name__ == '__main__':
